a084eee9223bc82198344fd035a6256ee717dd7f
nieledm <102603047+nieledm@users.noreply.github.com>
2026-05-11 11:49:30 -0300

    alterando porta ldap

--- app/auth.py
+++ app/auth.py
@@ -1,5 +1,6 @@
 import os
-from ldap3 import Server, Connection, ALL, SIMPLE
+import ssl
+from ldap3 import Server, Connection, ALL, SIMPLE, Tls
 
 # Se for rodar fora do Docker, descomente as linhas abaixo para carregar o .env
 # from dotenv import load_dotenv
@@ -15,11 +16,20 @@ def autenticar_e_obter_info(username, password):
         print("Erro: Variáveis de ambiente do LDAP não configuradas corretamente.")
         return None
 
-    # Monta a string no formato DOMINIO\usuario
     USER_DN = fr"{LDAP_DOMAIN}\{username}" 
 
+    tls_configuration = Tls(validate=ssl.CERT_NONE, version=ssl.PROTOCOL_TLSv1_2)
+
     try:
-        server = Server(LDAP_SERVER, get_info=ALL)
+        # 
+        server = Server(
+            LDAP_SERVER, 
+            port=636, 
+            use_ssl=True, 
+            tls=tls_configuration, 
+            get_info=ALL
+        )
+
         conn = Connection(server, user=USER_DN, password=password, authentication=SIMPLE)
         
         if conn.bind():
@@ -32,7 +42,6 @@ def autenticar_e_obter_info(username, password):
             
             if conn.entries:
                 entry = conn.entries[0]
-                # memberOf vem como uma lista de strings (DNs dos grupos)
                 grupos = entry.memberOf.values if 'memberOf' in entry else []
                 nome = str(entry.displayName) if 'displayName' in entry else username
                 conn.unbind()
